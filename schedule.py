3322b599483721f14681a0f831388b34f92431d4
James Koppen <jk@jameskoppen.com>
2015-01-29 22:55:02 -0600

    Now have an NBA on TNT calendar also.

--- schedule.py
+++ schedule.py
@@ -3,17 +3,26 @@
 from bs4 import BeautifulSoup as bs
 
 
-nba_tv_sched = bs(open("espn.go.com-nba-television.html"))
-schedule_table = nba_tv_sched.select("div#my-teams-table table")
+nba_tv_sched = bs(open("nbaontnt.html"))
+schedule_table = nba_tv_sched(id="nbaTNTsched")
 schedule = bs(str(schedule_table))
-table_rows = schedule.find_all('tr')
+table_rows = schedule('tr')
 games = [bs(str(game)) for game in table_rows]
 for game in games:
-    g = game.select('td')
-    if len(g) == 4:
-        if g[0].text in ["JANUARY","FEBRUARY","MARCH","APRIL"]:
-            month = g[0].text.capitalize()
-            continue
-        else:
-            print(g[0].text.split()[1] + " "+ month + " " + g[2].text
-                    + "\t" + g[1].text)
+    g = game('td')
+    date = g[0].text
+    print(date)
+    game1_details = g[1].text.split()
+    time1 = game1_details[0]
+    hour_minute = time1.split(":")
+    hour = hour_minute[0]
+    minute = hour_minute[1][:2]
+    print(" ".join(game1_details[1:]) + " " + hour + " " + minute)
+    if g[2].text:
+        game2_details = g[2].text.split()
+        time2 = game2_details[0]
+        hour_minute = time2.split(":")
+        hour = hour_minute[0]
+        minute = hour_minute[1][:2]
+        print(" ".join(game2_details[1:]) + " " + hour + " " + minute)
+
---
+++ tnt_ics.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+from bs4 import BeautifulSoup as bs
+from icalendar import Calendar, Event, vText
+from datetime import datetime, timedelta
+import pytz
+import os
+
+# throw out everything before February
+months = {"Feb": 2,
+          "Mar": 3,
+          "Apr": 4}
+cal = Calendar()
+cal.add('prodid', '-//NBA on TNT//http://www.nba.com/nbaontnt///')
+cal.add('version', '2.0')
+timezone = pytz.timezone("US/Eastern")
+
+def display_cal(cal):
+    return cal.to_ical().replace('\r\n', '\n').strip()
+
+def make_event(game, month, day):
+    game_details = game.text.split()
+    time = game_details[0]
+    hour_minute = time.split(":")
+    hour = int(hour_minute[0]) + 12
+    minute = int(hour_minute[1][:2])
+
+    duration = timedelta(minutes=25,hours=2)
+
+    start_time = datetime(2015,month,day,hour,minute,0)
+    end_time = start_time + duration
+
+    event = Event()
+    event.add('summary', " ".join(game_details[1:]))
+    event.add('dtstart', timezone.localize(start_time))
+    event.add('dtend', timezone.localize(end_time))
+    event.add('dtstamp', timezone.localize(datetime.now()))
+    event['location'] = vText(network)
+    if game.img:
+        event['url'] = url
+    return event
+
+# download of http://www.nba.com/nbaontnt/
+url = "http://www.nba.com/tntovertime/"
+network = "TNT"
+nba_tv_sched = bs(open("nbaontnt.html"))
+schedule_table = nba_tv_sched(id="nbaTNTsched")
+schedule = bs(str(schedule_table))
+table_rows = schedule('tr')
+games = [bs(str(game)) for game in table_rows]
+for game in games:
+    g = game('td')
+    date = g[0].text.split(", ")[1]
+    month_day = date.split()
+    if month_day[0] in months.keys():
+        month = months[month_day[0]]
+        day = int(month_day[1])
+        cal.add_component(make_event(g[1], month, day))
+        if g[2].text:
+            cal.add_component(make_event(g[2], month, day))
+
+
+#print(display_cal(cal))
+with open('nba_on_tnt.ics', 'wb') as f:
+    f.write(cal.to_ical())
