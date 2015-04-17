0c18b92ac0484cd14cf6c88584948b0b29f490ce
James Koppen <jk@jameskoppen.com>
2015-04-16 23:49:37 -0500

    Updated the NBA on ESPN calendar for the playoffs.

--- espn_ics.py
+++ espn_ics.py
@@ -6,12 +6,6 @@ from datetime import datetime, timedelta
 import pytz
 import os
 
-months = {
-        "JANUARY" : "1",
-        "FEBRUARY": "2",
-        "MARCH"   : "3",
-        "APRIL"   : "4"
-        }
 cal = Calendar()
 cal.add('prodid', '-//NBA on ESPN//espn.go.com/nba/television//')
 cal.add('version', '2.0')
@@ -20,43 +14,40 @@ def display_cal(cal):
     return cal.to_ical().replace('\r\n', '\n').strip()
 
 # download of http://espn.go.com/nba/television
-nba_tv_sched = bs(open("espn.go.com-nba-television.html"))
-schedule_table = nba_tv_sched.select("div#my-teams-table table")
+nba_tv_sched = bs(open("espn.go.com-nba-playoffs.html"))
+schedule_table = nba_tv_sched.select("tbody")
 schedule = bs(str(schedule_table))
 table_rows = schedule.find_all('tr')
 games = [bs(str(game)) for game in table_rows]
 for game in games:
     g = game.select('td')
     if len(g) == 4:
-        if g[0].text in ["JANUARY","FEBRUARY","MARCH","APRIL"]:
-            month = int(months[g[0].text])
-            continue
-        else:
-            day = int(g[0].text.split()[1])
+        month = 4
+        day = int(g[0].text.split()[1])
 
-            time_pm_tz = g[2].text.split()
-            split_t = time_pm_tz[0].split(":")
-            hour = int(split_t[0])
-            # assuming all games begin after noon
-            if hour < 12: hour += 12
-            minute = int(split_t[1])
+        time_pm_tz = g[2].text.split()
+        split_t = time_pm_tz[0].split(":")
+        hour = int(split_t[0])
+        # assuming all games begin after noon
+        if hour < 12: hour += 12
+        minute = int(split_t[1])
 
-            duration = timedelta(minutes=25,hours=2)
+        duration = timedelta(minutes=25,hours=2)
 
-            start_time = datetime(2015,month,day,hour,minute,0)
-            end_time = start_time + duration
+        start_time = datetime(2015,month,day,hour,minute,0)
+        end_time = start_time + duration
 
-            network = g[3].img['alt']
+        network = g[3].img['alt']
 
-            event = Event()
-            event.add('summary', g[1].text)
-            event.add('dtstart', timezone.localize(start_time))
-            event.add('dtend', timezone.localize(end_time))
-            event.add('dtstamp', timezone.localize(datetime.now()))
-            event['location'] = vText(network)
-            if network == "ESPN":
-                event['url'] = g[3].a['href']
-            cal.add_component(event)
+        event = Event()
+        event.add('summary', g[1].text)
+        event.add('dtstart', timezone.localize(start_time))
+        event.add('dtend', timezone.localize(end_time))
+        event.add('dtstamp', timezone.localize(datetime.now()))
+        event['location'] = vText(network)
+        if "ESPN" in network:
+            event['url'] = g[3].a['href']
+        cal.add_component(event)
 
 #print(display_cal(cal))
 with open('nba_on_espn.ics', 'wb') as f:
