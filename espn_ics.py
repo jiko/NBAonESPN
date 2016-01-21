cb037abcc7949a133e216ea49790b6d6a826dc7c
James Koppen <jk@jameskoppen.com>
2016-01-21 16:07:27 -0600

    Update the script for the new schedule page

--- espn_ics.py
+++ espn_ics.py
@@ -6,32 +6,32 @@ from bs4 import BeautifulSoup as bs
 from icalendar import Calendar, Event, vText
 
 cal = Calendar()
-cal.add('prodid', '-//NBA on ESPN//espn.go.com/nba/television//')
+cal.add('prodid', '-//NBA on ESPN//espn.go.com/nba/schedule//')
 cal.add('version', '2.0')
 def display_cal(cal):
     return cal.to_ical().replace('\r\n', '\n').strip()
 
-r = get("http://espn.go.com/nba/television")
-nba_tv_sched = bs(r.text)
-schedule_table = nba_tv_sched.select("table.tablehead")
-schedule = bs(str(schedule_table))
-table_rows = schedule.find_all('tr')
-for game in table_rows:
-    if game['class'][0] != "colhead" and game['class'][0] != "stathead":
-        g = game.select('td')
-        start_time = time(" ".join(["May", g[0].text, g[2].text]), tz='America/New_York')
-        duration = delta(minutes=30,hours=2)
-        end_time = start_time + duration
+r = get("http://espn.go.com/nba/schedule")
+nba_tv_sched = bs(r.text, 'html.parser')
+watch_espn = nba_tv_sched.select('img[alt=WatchESPN]')
+for game in watch_espn:
+    row = game.parent.parent.parent
+    cells = row.select('td')
+    away = cells[0].select('a > abbr')[0]['title']
+    home = cells[1].select('a > abbr')[0]['title']
+    start = cells[2]['data-date'] # in GMT
+    start_time = time(start)
+    duration = delta(minutes=30,hours=2)
+    end_time = start_time + duration
+    url = cells[3].select('a')[1]['href']
 
-        event = Event()
-        event.add('summary', g[1].text)
-        event.add('dtstart', start_time.datetime)
-        event.add('dtend', end_time.datetime)
-        event.add('dtstamp', time().datetime)
-        network = g[3].img['alt']
-        event['location'] = vText(network)
-        if "ESPN" in network:
-            event['url'] = g[3].a['href']
-        cal.add_component(event)
+    event = Event()
+    event.add('summary', away + ' at ' + home)
+    event.add('dtstart', start_time.datetime)
+    event.add('dtend', end_time.datetime)
+    event.add('dtstamp', time().datetime)
+    event['location'] = vText('WatchESPN')
+    event['url'] = url
+    cal.add_component(event)
 
 print(display_cal(cal))
